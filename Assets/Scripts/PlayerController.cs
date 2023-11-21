dcbddd61ffa0085bf818bbd501f4221b127c3f9e
Will Bohlke <willbohlke@gmail.com>
2023-11-21 18:37:17 -0500

    made time
    *created a countdown timer, hoping to trigger a jump animation once it reaches 0

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,13 +10,17 @@ public class PlayerController : MonoBehaviour
 
     private Vector3 startPos;
     private Rigidbody playerRb;
+    private float yBound = 25.0f;
+    private Animator playerAnim;
 
     // Start is called before the first frame update
     void Start()
     {
         isOnGround = false;
         playerRb = GetComponent<Rigidbody>();
+        playerAnim = GetComponent<Animator>();
         Physics.gravity *= gravityMultiplier;
+
     }
 
     // Update is called once per frame
@@ -24,9 +28,17 @@ public class PlayerController : MonoBehaviour
     {
         float verticalInput = Input.GetAxis("Vertical");
 
+        // make player jump after countdown says "Go!"
+
+
         // make player thrust upward if spacebar is pressed and they havent touched the ground yet
         if (!isOnGround) {
             playerRb.AddForce(Vector3.up * thrustForce * verticalInput);
+        }
+
+        // constrain player to not go over a certain height
+        if (transform.position.y > yBound) {
+            transform.position = new Vector3(transform.position.x, yBound, transform.position.z);
         }
     }
 
@@ -36,5 +48,7 @@ public class PlayerController : MonoBehaviour
             Debug.Log("Game Over!");
         }
     }
+
+    
 
 }
